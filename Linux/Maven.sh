4857712286a29a4a2f5506f9a97c42f2efbf7254
Ivan.Kryskun <ivan.kriskun@gmail.com>
2021-02-16 09:25:50 +0200

    Added maven configuration

--- Linux/Maven.sh
+++ Linux/Maven.sh
@@ -12,3 +12,15 @@ sudo vi /etc/profile.d/maven.sh
 #export PATH=${M2_HOME}/bin:${PATH}
 source /etc/profile.d/maven.sh
 mvn -version
+# Add the folder where maven will build projects
+# edit file /home/ec2-user/maven/apache-maven-3.6.3/conf/settings.xml
+# Add the end of the section
+#  <!-- localRepository
+#   | The path to the local repository maven will use to store artifacts.
+#   |
+#   | Default: ${user.home}/.m2/repository
+#  <localRepository>/path/to/local/repo</localRepository>
+#  -->
+# Copy  <localRepository>/path/to/local/repo</localRepository> modify it with real path and insert blow the "-->"
+# Example
+#  <localRepository>/home/ec2-user/maven/</localRepository>
